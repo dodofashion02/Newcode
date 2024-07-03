eeae0b2d82bd09c1317ba884bc6914cf1ec42144
dodofashion02 <info.dodofashion@gmail.com>
2024-07-03 15:16:08 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,12 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const Product = require('./models/productModel'); // Import the Product model
 const UpiData = require('./models/upiData'); // Import the UPI data model
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://flipfrontend-asdnl2vbo-dodo-fashions-projects.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 const app = express();
 const PORT = process.env.PORT || 5000;
 
