662124e90ff72069a11ebddc8e695fdb05e38d7b
dodofashion02 <info.dodofashion@gmail.com>
2024-07-03 15:26:00 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -6,6 +6,7 @@ const UpiData = require('./models/upiData'); // Import the UPI data model
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.use(cors());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://flipfrontend-asdnl2vbo-dodo-fashions-projects.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
@@ -13,8 +14,6 @@ app.use((req, res, next) => {
     next();
 });
 
-// Middleware
-app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
