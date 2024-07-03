7e311f4dc190e957b9a0192d3cbc260adc6131e8
dodofashion02 <info.dodofashion@gmail.com>
2024-07-03 23:37:11 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -8,11 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-    origin: 'https://serverfinal-n1ys.onrender.com',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
