d1ab26f5961adccba6c52d3b38513e4cb0fb758d
dodofashion02 <info.dodofashion@gmail.com>
2024-07-03 22:55:55 +0530

    Update config.js

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,4 +1,4 @@
 // config.js
-const apiUrl = 'https://newcode-9jfh30jul-dodo-fashions-projects.vercel.app'; // Replace with your actual API base URL
+const apiUrl = 'https://serverfinal-n1ys.onrender.com'; // Replace with your actual API base URL
 export default apiUrl;
    
